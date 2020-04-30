89acf0e39c5c3d02184c99e69b507dc2d883a0ca
Louis Thackray <louis.thackray@phe.gov.uk>
2020-04-30 08:53:20 +0100

    First commit 30th April

---
+++ mailing_address.py
@@ -0,0 +1,30 @@
+
+# Solution 1: Mailing Address
+
+# This solution at https://www.w3resource.com/python-exercises/python-basic-exercise-37.php
+
+
+def personal_details():
+    name = "Simon Diamond"
+    age = 19
+    address = "Bangalore, Karnataka, India"
+    print("Name: {}\nAge:{}\nAddress: {}".format(name, age, address))
+
+personal_details()
+
+print() # leaves one line blank when printing out
+
+
+# My attempt
+
+def my_details():
+    name = "Adrian Mole"
+    age = 13
+    address = "Someplace in the World"
+    postcode = "ABC 123"
+    print("Name: {}\nAge:{}\nAddress: {}\nPostcode:{}".format(name, age, address, postcode))
+
+my_details()
+
+
+
